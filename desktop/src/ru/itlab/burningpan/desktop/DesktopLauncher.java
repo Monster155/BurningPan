9dcb8f7d767a841ebc7e7c84aeed7226198f6ec1
Damir D <damir180501@mail.ru>
2019-11-12 16:54:45 +0300

    Initial commit

---
+++ desktop/src/ru/itlab/burningpan/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.itlab.burningpan.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.itlab.burningpan.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
